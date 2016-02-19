91bd5b2aead24f3a8903fceca9995c01b6ec3e18
Abe Hiroaki <nananeko@iit-ad.iit.jp>
2016-02-19 15:50:17 +0900

    fix to save extracted image file

--- src/lib/loop.py
+++ src/lib/loop.py
@@ -7,15 +7,33 @@ import re
 import requests
 
 # 縮小済みの画像(256*256)を取得
+# Put + 1 => bg の番号
+
+# コマンドのヘルプ
+# ExtractObjectBoxRegion.exe whole_input_image object_image output_image
+# コマンド実行のサンプル
+# ExtractObjectBoxRegion.exe ..\bg_0002837.png ..\putobject_0002836_027.png ..\output.png
 def getUnMaskedImage(filepath_img_masked,dict_conf):
-    imgname_masked=os.path.basename(filepath_img_masked)
-    imgname_unmasked=re.match("_[0-9]+_",imgname_masked).group(1)[1:-1]+myutils.get_ext(imgname_masked)
-    imgpath_unmasked=os.path.join(os.dirname(dict_conf["table_object_manager"]["output_rawimage"]),imgname_unmasked)
-    imgdir_output=os.path.dirname(filepath_img_masked).split("/")[-1]
-    imgdir_output_base="".join(dict_conf["object_region_box_extractor"]["output_touch"].split("/")[:-2])
-    imgpath_ouput=os.path.join(imgdir_output_base,imgdir_output,imfname_unmasked)
-    list_imgpath=[imgpath_unmasked,filepath_img_masked,imgpath_output]
-    list_opt=[myutils.convert_to_cygpath(filepath_img)]+list_imgpath+dict_conf["object_region_box_extractor"]["default_options"].split()
+    # %output_root%\%SESSION_ID%
+    base_dir = os.path.join(dict_conf["chiffon_client"]["output_root"],dict_conf["session_id"])
+
+    # マスク済みファイル名からフレーム数を抜き出し、背景差分の元情報となるファイル名を取得する。
+    maskedimage_filename = os.path.basename(filepath_img_masked)
+    pattern=re.compile("(_[0-9]+_)")
+    frame_number_astext = pattern.search(maskedimage_filename).group(1)[1:-1]
+    number_length = len(frame_number_astext)
+    frame_number = str(int(frame_number_astext) + 1) # 背景画像は次の番号になる。
+
+    bgimage_filename = "bg_" + frame_number.zfill(number_length) + myutils.get_ext(maskedimage_filename)
+    bgimage_path=os.path.join(dict_conf["table_object_manager"]["output_rawimage"],bgimage_filename)
+
+    # 縮小済み画像の出力先パス・ファイル名の作成
+    extractor_path=os.path.join(base_dir, dict_conf["object_region_box_extractor"]["output_touch"])
+    imgpath_output=os.path.join(extractor_path, maskedimage_filename)
+
+    # 実行
+    list_imgpath=[bgimage_path, filepath_img_masked, imgpath_output]
+    list_opt = list_imgpath + dict_conf["object_region_box_extractor"]["default_options"].split()
     imgpath_output=myutils.callproc_cyg(dict_conf["object_region_box_extractor"]["path_exec"],list_opt)
     return imgpath_output
 
@@ -40,6 +58,10 @@ def featureExtraction(filepath_img,dict_conf):
     list_path_images=myutils.get_files_from_exts(abspath_dir_image,dict_conf["table_object_manager"]["fileexts"])
     result_feature=make_results_FE(list_path_images,dict_conf)
     print("Feature extraction has been completed.")
+    # TODO save feature as file
+    # TODO need to define file name
+    print(result_feature)
+
     return result_feature
 
 
@@ -51,7 +73,7 @@ def make_dict_query_s4r(filepath_img,feature_extracted,dict_conf):
     dict_json_data={"feature":feature_extracted,"id:":dir_img,"group":list_group,"name":dict_conf["recipe_id"]}
     dict_query={"json_data":json.dumps(dict_json_data)}
     return dict_query
-    
+
 
 def make_url_server4recog(filepath_img,feature_extracted,dict_conf):
     dict_query=make_dict_query_s4r(filepath_img,feature_extracted,dict_conf)
